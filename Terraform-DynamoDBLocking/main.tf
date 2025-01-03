2db60f6e17afcb7d072031266f43aceea48c15e9
Srikanth-Nalabothula <srikanthnalabothula55@gmail.com>
2025-01-03 12:37:05 -0500

    removing dynamodb state locking to progress with the poc

--- Terraform-DynamoDBLocking/main.tf
+++ Terraform-DynamoDBLocking/main.tf
@@ -7,10 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "srikanth-terraform"
-    key            = "workspace.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "dynamodb-state-locking"
+    bucket = "srikanth-terraform"
+    key    = "workspace.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = "dynamodb-state-locking"
   }
 }
 
